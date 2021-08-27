b954d1ac77e1b26e01715a3f52e1ef1b110446e9
lukostelloe <lukostelloe@gmail.com>
2021-08-27 11:01:41 +0200

    aasss

--- modal.js
+++ modal.js
@@ -3,6 +3,13 @@ var span = document.getElementsByClassName("close")[0];
 var yes = document.getElementById("yes");
 var cancel = document.getElementById("cancel");
 
+var carbutton = document.getElementById("addcarbutton");
+var status = document.getElementById("confirmation");
+
+carbutton.addEventListener("click", function () {
+  status.innerHTML = "car added!";
+});
+
 // When the user clicks on (x) or cancel, close the modal
 span.onclick = function () {
   modal.style.display = "none";
